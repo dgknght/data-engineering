3410e90818582b1951dc7dd4a630fd2239f140c4
Doug Knight <administrator@knightsoftwareservices.com>
2014-02-19 15:33:05 -0600

    Added initial routes for orders, HAML gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 DataEngineering::Application.routes.draw do
+  get "orders/index"
+  resources :orders, only: [:index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
